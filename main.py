837a62325fce7988d9fffe090a6a957efc007ab0
Edson de Jesus <edson.intrapessoal@gmail.com>
2025-07-11 16:21:04 -0300

    Corrigido requirements.txt e pacote helpers

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
-from fastapi import FastAPI, Query
-import random
-import json
-from filtros import aplicar_filtros
+from fastapi import FastAPI
+from helpers.gerador import gerar_combinacoes
 
 app = FastAPI()
 
 @app.get("/")
-def home():
+def raiz():
     return {"mensagem": "API do Código da Sorte Online."}
 
 @app.get("/gerar")
-def gerar_combinacoes(quantidade: int = Query(..., gt=0, le=10000)):
-    with open("config.json", "r") as f:
-        config = json.load(f)
-
-    resultados = []
-    tentativas = 0
-
-    while len(resultados) < quantidade and tentativas < quantidade * 50:
-        jogo = sorted(random.sample(config["dezenas_validas"], 15))
-        if aplicar_filtros(jogo, config):
-            resultados.append(jogo)
-        tentativas += 1
-
-    return {
-        "total_gerado": len(resultados),
-        "tentativas": tentativas,
-        "combinacoes": resultados
-    }
+def gerar():
+    combinacoes = gerar_combinacoes()
+    return {"jogos": combinacoes}
