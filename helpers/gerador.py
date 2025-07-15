98e3d403b5b938cef920c573e3b6ef04b0649772
Edson de Jesus <edson.intrapessoal@gmail.com>
2025-07-14 22:52:05 -0300

    AtualizaþÒo dißria dos filtros e jogos

--- helpers/gerador.py
+++ helpers/gerador.py
@@ -3,11 +3,11 @@ from itertools import combinations
 # ================================ #
 # CONFIGURAÇÃO DOS FILTROS FIXOS  #
 # ================================ #
-pares_desejados = 9
-impares_desejados = 6
+pares_desejados = 7
+impares_desejados = 8
 
-grupo1 = [1,2,3,4,5,11,14,16,18,20,21,22,23,24,25]
-grupo2 = [6,7,8,9,10,12,13,15,17,19]
+grupo1 = [1,3,5,6,7,9,10,13,15,16,18,19,21,23,25]
+grupo2 = [2,4,8,11,12,14,17,20,22,24]
 
 excluir_numeros = [16,17]
 exigir_numeros = [8]
