35b251286f717428f5185673263ff4f8bda1bdf9
Edson de Jesus <edson.intrapessoal@gmail.com>
2025-12-09 11:23:51 -0300

    AtualizaþÒo dißria dos filtros e jogos

--- helpers/gerador.py
+++ helpers/gerador.py
@@ -7,15 +7,15 @@ from itertools import combinations
 pares_desejados = 7
 impares_desejados = 8
 
-grupo1 = [1,2,5,8,11]
-grupo2 = [12,13,15,18,19]
-grupo3 = [21,22,23,24,25]
-grupo4 = [3,4,6,7,9,10,14,16,17,20]
+grupo1 = [3,8,13,18]
+grupo2 = [14,15,16,17,19]
+grupo3 = [5,6,7,9,10,20,24]
+grupo4 = [1,2,4,11,12,25]
 
 quantidade_g1 = 3
 quantidade_g2 = 3
-quantidade_g3 = 3
-quantidade_g4 = 6
+quantidade_g3 = 4
+quantidade_g4 = 5
 
 # === Funções auxiliares ===
 
